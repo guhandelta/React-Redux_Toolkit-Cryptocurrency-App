2661deea14566fcc5754bd2f3be3c8601b76699b
Kala Foods <guhan.delta@gmail.com>
2021-09-22 22:04:39 -0400

    Configured the UI to display teh Global stats. Currently no data available sue to issues with API on RapidAPI

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,27 +2,35 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 
+import { useGetCryptosQuery } from '../services/cryptoApi'
+
 const { Title } = Typography;
 
 const Homepage = () => {
+
+    const { data, isFetching } = useGetCryptosQuery();
+    const GlobalStatsData = data?.data?.stats;
+
+    if(isFetching) return 'Loading...';
+
     return (
         <>
             <Title level={2} className="heading">Global Cryptocurrency stats</Title>
             <Row>
                 <Col span={12}>
-                    <Statistic value="5" title="Total Cryptocurrencies" />
+                    <Statistic value={GlobalStatsData?.total} title="Total Cryptocurrencies" />
                 </Col>
                 <Col span={12}>
-                    <Statistic value="5" title="Total Exchanges" />
+                    <Statistic value="5"/*{millify(GlobalStatsData?.totalExchanges)}*/ title="Total Exchanges" />
                 </Col>
                 <Col span={12}>
-                    <Statistic value="5" title="Total Market Cap" />
+                    <Statistic value="5"/*{millify(GlobalStatsData?.totalMarketCap)}*/ title="Total Market Cap" />
                 </Col>
                 <Col span={12}>
-                    <Statistic value="5" title="Total 24h Volume" />
+                    <Statistic value="5"/*{millify(GlobalStatsData?.total24hVolume)}*/ title="Total 24h Volume" />
                 </Col>
                 <Col span={12}>
-                    <Statistic value="5" title="Total Markets" />
+                    <Statistic value="5"/*{millify(GlobalStatsData?.totalMarkets)}*/ title="Total Markets" />
                 </Col>
             </Row>
         </>
