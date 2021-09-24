ece60711f4a30802fbf59d4c046e93e23811fc3d
Kala Foods <guhan.delta@gmail.com>
2021-09-24 18:44:55 -0400

    Ported the keys to .env

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const { CRYPTO_API_HOST, CRYPTO_API_API_KEY } = process.env;
+
 // createApi(): The core of RTK Query's functionality. It allows you to define a set of endpoints describe how to retrieve data from a series of endpoints, including configuration of how to fetch and transform that data. In most cases, you should use this once per app, with "one API slice per base URL" as a rule of thumb.
 // fetchBaseQuery(): A small wrapper around fetch that aims to simplify requests. Intended as the recommended baseQuery to be used in createApi for the majority of users.
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '13e2810994msh948dfb418dee1e4p1ef656jsn70c2441337be'
+    'x-rapidapi-host': CRYPTO_API_HOST,
+    'x-rapidapi-key': CRYPTO_API_API_KEY
 }
 
 const baseUrl =  'https://coinranking1.p.rapidapi.com';
