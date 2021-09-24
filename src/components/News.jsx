962cf17c2a35cf642e457c34f810d92913c6426b
Kala Foods <guhan.delta@gmail.com>
2021-09-24 09:24:03 -0400

    Added Select option to Crypto News page to allow filter news as per the desired cryptocurrency

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,10 @@
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
 import moment from 'moment';
 import { useState } from 'react';
+import { Cryptocurrencies } from '.';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -10,8 +12,10 @@ const { Option } = Select;
 const demoImageUrl = "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
 
 const News = ({ simplified }) => {
-
-    const {data: cryptoNews} = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 6 : 12 });
+    
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const { data } = useGetCryptosQuery(100);
+    const {data: cryptoNews} = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
 
     if(!cryptoNews?.value) return 'Loading...';
 
@@ -20,33 +24,53 @@ const News = ({ simplified }) => {
 
 
     return (
-        <Row gutter={[24, 24]}>
-            {cryptoNews.value.map((news, i)=>(
-                <Col xs={24} lg={12} sm={8} key={i}>
-                    <Card hoverable className="news-card">
-                        <a href={news.url} target="_blank" rel="noreferrer">
-                            <div className="news-image-container">
-                                <Title className="news-title" level={4}>{news.name}</Title>
-                                <img style={{ maxWidth: '100px', maxHeight:'100px' }} src={news?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
-                            </div>
-                            <p>
-                                {news.description > 100
-                                    ? `${news.description.substring(0,100)}...`
-                                    : news.description
-                                }
-                            </p>
-                            <div className="provider-container">
-                                <div>
-                                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
-                                    <Text className="provider-name">{news.provider[0]?.name}</Text>
+        <>
+            <Row gutter={[24, 24]}>
+                {!simplified &&(
+                    <Col span={24}>
+                        <Select
+                            showSearch
+                            className="select-news"
+                            placeholder="Search Crypto"
+                            optionFilterProp="children"
+                            onChange={value => setNewsCategory(value)}
+                            // fiter out the options to show only the desired crypto currency
+                            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                        >
+                            <Option value="Cryptocurrency">Cryptocurrency</Option>
+                            {data?.data?.coins.map(coin => <Option value={coin.name}>{coin.name}</Option>)}
+                        </Select>
+                    </Col>
+                )}
+            </Row>
+            <Row gutter={[24 ,24]} style={{marginTop:'2em'}}>
+                {cryptoNews.value.map((news, i)=>(
+                    <Col xs={24} sm={12} lg={8} key={i}>
+                        <Card hoverable className="news-card">
+                            <a href={news.url} target="_blank" rel="noreferrer">
+                                <div className="news-image-container">
+                                    <Title className="news-title" level={4}>{news.name}</Title>
+                                    <img style={{ maxWidth: '100px', maxHeight:'100px' }} src={news?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
                                 </div>
-                                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
-                            </div>
-                        </a>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+                                <p>
+                                    {news.description > 100
+                                        ? `${news.description.substring(0,100)}...`
+                                        : news.description
+                                    }
+                                </p>
+                                <div className="provider-container">
+                                    <div>
+                                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
+                                        <Text className="provider-name">{news.provider[0]?.name}</Text>
+                                    </div>
+                                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                                </div>
+                            </a>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -20,7 +20,7 @@ export const cryptoNewsApi = createApi({
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
             // To make this request, the header should also be passed. The URL and the header are added to teh call using an utility fn() 
-            query: ({ newsCategody, count }) => createRequest(`/news/search?q=${newsCategody}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
         })
     })   
 })
