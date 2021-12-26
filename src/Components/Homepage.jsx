62a213e026bfab3ab9d346f17bbb299b9aeabe2d
Kala Foods <guhan.delta@gmail.com>
2021-12-26 13:33:27 -0500

    Created the basic structure of the Homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,7 +1,29 @@
+import millify from "millify"
+import { Typography, Row, Col, Statistic } from 'antd'
+
+const { Title } = Typography;
+
 const Homepage = () => {
     return (
         <div>
-            <h1>Crypto உலகம்</h1>
+            <Title>Global Crpto Statistics</Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic title="Total Cryptocurrencies" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Exchanges" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Market Cap" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total 24h Volume" value="5" />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Markets" value="5" />
+                </Col>
+            </Row>
         </div>
     )
 }
