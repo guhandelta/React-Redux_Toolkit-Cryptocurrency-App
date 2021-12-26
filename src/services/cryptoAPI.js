59ffb3165a622c44bd1343e5144aa285b05644b2
Kala Foods <guhan.delta@gmail.com>
2021-12-26 16:50:37 -0500

    Created the endpoint to fetch Crypto currency data and connected it with the store

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -8,3 +8,20 @@ const cryptoAPIHeaders = {
   'x-rapidapi-host': CRYPTO_API_HOST,
   'x-rapidapi-key': CRYPTO_API_API_KEY
 }
+
+// Utility fn() to add URL and the corresponding header
+const createRequest = (url) => ({ url, headers: cryptoAPIHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi', // What is this reducer for
+    baseQuery: fetchBaseQuery({ baseURL }),
+
+    // Endpoints = A fn(), where a builder is available as the 1st param, which is fn() that returns an obj, within which the names of the-
+    //- endpoints can be mentioned/specified
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            // To make this request, the header should also be passed. The URL and the header are added to the call using an utility fn() 
+            query: count => createRequest(`/coins?limit=${count}`)
+        }),
+    })
+});
