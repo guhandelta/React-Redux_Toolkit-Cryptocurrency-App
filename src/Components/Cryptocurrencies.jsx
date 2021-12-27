02d376ea9c83942b8ab1c179f0c1031fbe024414
Kala Foods <guhan.delta@gmail.com>
2021-12-26 23:03:59 -0500

    Populated the /cryptocurrencies page with the crypto data on separate cards

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -1,10 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import millify from "millify";
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
 
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
+
+    const { data:cryptosList, isFetching } = useGetCryptosQuery();
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+    console.log(cryptos);
+
     return (
-        <div>
-            <h1>Cryptocurrencies</h1>
-        </div>
+        <>
+            <Row  gutter={[32, 32]} className="crypto-card-container">
+                {cryptos.map(crypto => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
+                       <Link to={`/crypto/${crypto.id}`}>
+                        <Card 
+                                title={`${crypto.rank}. ${crypto.name}`}
+                                extra={<img className="crypto-image" src={crypto.iconUrl} alt={crypto.name} />}
+                                hoverable
+                            >
+                                <p>Price: {millify(crypto.price)}</p>
+                                <p>Market Cap: {millify(crypto.marketCap)}</p>
+                                <p>Daily Change: {millify(crypto.change)}%</p>
+                            </Card>
+                       </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
