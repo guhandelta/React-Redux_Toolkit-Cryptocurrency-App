56b147da9ced76a230e8d0aca640c6fddce9cd2d
Kala Foods <guhan.delta@gmail.com>
2021-12-29 11:29:51 -0500

    Added a Dropdown with the coins, to filter the News content, on /News

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,26 +1,50 @@
 import { Select, Typography, Avatar, Card, Row, Col } from 'antd';
 import moment from 'moment';
+import { useState } from 'react';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
 const News = ({ simplified }) => {
+
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
     
-    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 21 });
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 21 });
+    const { data } = useGetCryptosQuery(100);
 
     if(!cryptoNews?.value) return 'Loading...';
 
-    console.log('====================================');
     console.log('cryptoNews: ', cryptoNews);
-    console.log('====================================');
 
     // const backupImage = 'https://www.cryptocompare.com/media/20646/default-avatar.png';
     const backupImage = 'https://www.coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg';
-
+    // console.log('data: ', data);
+    // console.log('data.data: ', data.data);
+    // console.log('data.data.coins: ', data.data.coins);
+    // console.log('data.data.coins.name: ', data.data.coins.name);
     return (
         <Row gutter={[32, 32]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select 
+                        showSearch
+                        className="select-news" 
+                        placeholder="Select a Cryptocurrecny"
+                        optionFilterProp="children"	
+                        onChange={(value) => setNewsCategory(value)}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                    >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => { 
+                            return <Option key={coin.id} value={coin.name}>{coin.name}</Option>
+                        })}
+                    </Select>
+
+                </Col>
+            )}
             {cryptoNews.value.map(news => (
                 <Col xs={24} sm={12} lg={8} key={news.id} span={24}>
                     <Card hoverable className="news-card">
